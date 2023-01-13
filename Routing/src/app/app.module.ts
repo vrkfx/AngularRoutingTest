b5081ec997b29fc92b916af0156f95d37e2fba7f
Virroshan_Kumar <virroshan08@outlook.com>
2023-01-13 14:32:11 +1030

    Add basic routing - Working

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
+
+//Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { TestComponent } from './test/test.component';
+//Routes Definition
+
+const appRoute: Routes = [
+  // {path: '', component:HomeComponent},
+  {path: 'Home', component:HomeComponent},
+  //path match tells url how ot match the router and full is matched to the entire URL
+  {path: '', redirectTo:'Home', pathMatch:'full'},
+  {path: 'Products', component:ProductsComponent},
+  {path: 'Test', component:TestComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    TestComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
